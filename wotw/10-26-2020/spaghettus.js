2b56d8d837fdc4d0e8cbe9988e46a4aad07e10ec
1ctinus <72636637+1ctinus@users.noreply.github.com>
2020-10-26 12:51:08 -0500

    Add files via upload

---
+++ wotw/10-26-2020/spaghettus.js
@@ -0,0 +1,137 @@
+
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 65) {
+        window.location.replace("a.html");
+    }
+});
+
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 66) {
+        window.location.replace("b.html");
+    }
+});
+
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 67) {
+        window.location.replace("c.html");
+    }
+});
+
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 68) {
+        window.location.replace("d.html");
+    }
+});
+
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 69) {
+        window.location.replace("e.html");
+    }
+});
+
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 70) {
+        window.location.replace("f.html");
+    }
+});
+
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 71) {
+        window.location.replace("g.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 72) {
+        window.location.replace("h.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 73) {
+        window.location.replace("i.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 74) {
+        window.location.replace("j.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 75) {
+        window.location.replace("k.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 76) {
+        window.location.replace("l.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 77) {
+        window.location.replace("m.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 78) {
+        window.location.replace("n.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 79) {
+        window.location.replace("o.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 80) {
+        window.location.replace("p.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 81) {
+        window.location.replace("q.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 82) {
+        window.location.replace("r.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 83) {
+        window.location.replace("s.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 84) {
+        window.location.replace("t.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 85) {
+        window.location.replace("u.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 86) {
+        window.location.replace("v.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 87) {
+        window.location.replace("w.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 88) {
+        window.location.replace("x.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 89) {
+        window.location.replace("y.html");
+    }
+});
+document.body.addEventListener("keydown", function (event) {
+    if (event.keyCode === 90) {
+        window.location.replace("z.html");
+    }
+});
